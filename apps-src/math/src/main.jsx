f3b07cf9a21bd0e9df9f59681c90704e1cf85a70
razed-developer <razed-developer@outlook.com>
2026-08-10 16:47:02 -0700

    Add function operations lab

--- apps-src/math/src/main.jsx
+++ apps-src/math/src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './header.css';
+import FunctionOperationsLab from './components/FunctionOperationsLab';
+
+const isFunctionsRoute = window.location.pathname.replace(/\/$/,'').endsWith('/math/functions');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>{isFunctionsRoute ? <FunctionOperationsLab /> : <App />}</React.StrictMode>
 );
